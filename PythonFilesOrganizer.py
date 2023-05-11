3857024c8393f395333535210709d59804a53a98
Rodrigo Morais <rodrigomiguelmorais@gmail.com>
2023-05-11 22:18:32 +0100

    Create PythonFilesOrganizer.py
    Done

---
+++ PythonFilesOrganizer.py
@@ -0,0 +1,47 @@
+#Organize files script
+import os
+import shutil
+
+def organize_directory(directory):
+    if not os.path.exists(directory):
+        print("Directory does not exist.")
+        return
+
+    extensions = set()
+
+    items = os.listdir(directory)
+
+    for item in items:
+        #gets item path directory + item ex Desktop\abcd.pdf
+        item_path = os.path.join(directory, item)
+        #check if is a file
+        if os.path.isfile(item_path):
+            base_name, extension = os.path.splitext(item)
+            extension = extension[1:]
+
+            extensions.add(extension)
+
+            #Ex: My xlsx Files
+            folder_name = "My {} Files".format(extension.upper())
+            #ex: Desktop\My xlsx Files
+            folder_path = os.path.join(directory, folder_name)
+            #if !Desktop\My xlsx Files makes it
+            if not os.path.exists(folder_path):
+                os.mkdir(folder_path)
+
+            destination_path = os.path.join(folder_path, item)
+            #Try move it if !move log the exception
+            try:
+                shutil.move(item_path, destination_path)
+                print("Moved '{}' to '{}'".format(item_path, destination_path))
+            except Exception as e:
+                print("Error moving '{}': {}".format(item_path, str(e)))
+
+    print("Organization completed.")
+
+directory = '../'
+directory = os.path.join(directory, input("Enter the name of the directory to organize (ex: Desktop, Documents) or Folder ex(Documents/FolderName): "))
+
+organize_directory(directory)
+
+
